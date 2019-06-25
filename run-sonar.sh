702ab0558f19195e9b419f1f0a2b3103a2233eca
Shpetim Selaci <shselaci3@gmail.com>
2019-06-25 20:56:34 +0200

    small linting

--- run-sonar.sh
+++ run-sonar.sh
@@ -3,39 +3,39 @@
 set -eu
 
 if [ -n "${SONAR_TOKEN:-}" ]; then
-  SONAR_OPTS="${SONAR_OPTS} -D sonar.login=${SONAR_TOKEN}"
+    SONAR_OPTS="${SONAR_OPTS} -D sonar.login=${SONAR_TOKEN}"
 fi
 
 if [ -n "${SONAR_HOST:-}" ]; then
-  SONAR_OPTS="${SONAR_OPTS} -D sonar.host.url=${SONAR_HOST}"
+    SONAR_OPTS="${SONAR_OPTS} -D sonar.host.url=${SONAR_HOST}"
 fi
 
 if [ -n "${SONAR_PROJECT_KEY:-}" ] then
-  SONAR_OPTS="${SONAR_OPTS} -d sonar.projectKey=${SONAR_PROJECT_KEY}"
+    SONAR_OPTS="${SONAR_OPTS} -d sonar.projectKey=${SONAR_PROJECT_KEY}"
 fi
 
 if [ -n "${SONAR_SOURCES:-}" ] then
-  SONAR_OPTS="${SONAR_OPTS} -d sonar.sources=${SONAR_SOURCES}"
+    SONAR_OPTS="${SONAR_OPTS} -d sonar.sources=${SONAR_SOURCES}"
 fi
 
 if [ -n "${SONAR_ORGANIZATION:-}" ] then
-  SONAR_OPTS="${SONAR_OPTS} -d sonar.organization=${SONAR_ORGANIZATION}"
+    SONAR_OPTS="${SONAR_OPTS} -d sonar.organization=${SONAR_ORGANIZATION}"
 fi
 
 if [ -n "${CIRCLE_SHA1:-}" ]; then
-  SONAR_OPTS="${SONAR_OPTS} -D sonar.projectVersion=${CIRCLE_SHA1}"
+    SONAR_OPTS="${SONAR_OPTS} -D sonar.projectVersion=${CIRCLE_SHA1}"
 fi
 
 if [ -n "${CIRCLE_PR_NUMBER:-}" ]; then
-  SONAR_OPTS="${SONAR_OPTS} -D sonar.analysis.mode=preview -D sonar.pullrequest.key=${CIRCLE_PR_NUMBER}"
+    SONAR_OPTS="${SONAR_OPTS} -D sonar.analysis.mode=preview -D sonar.pullrequest.key=${CIRCLE_PR_NUMBER}"
 fi
 
 if [ -n "${CIRCLE_REPOSITORY_URL:-}" ]; then
-  SONAR_OPTS="${SONAR_OPTS} -D sonar.links.scm=${CIRCLE_REPOSITORY_URL}"
+    SONAR_OPTS="${SONAR_OPTS} -D sonar.links.scm=${CIRCLE_REPOSITORY_URL}"
 fi
 
 if [ -n "${SONAR_OPTS:-}" ]; then
-  sonar-scanner $SONAR_OPTS
+    sonar-scanner $SONAR_OPTS
 else
-  sonar-scanner
+    sonar-scanner
 fi
